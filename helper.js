6dd9a8be44350295d785b190d49093242a0f481d
bhasker-qocial <97150566+bhasker-qocial@users.noreply.github.com>
2022-09-24 10:18:27 +0530

    function refactor

--- helper.js
+++ helper.js
@@ -15,12 +15,45 @@ export const RemoveEmptyValueFromArray = (param) => {
   }
 }
 
-export const binToHex = (str) => {
-    let decimal = parseInt(str, 2);
-    return decimal.toString(16);
+export const binToHex = (str) =>{
+    
+    if(str.length !== 96){
+        return false;
+    }
+    
+    let hex = "";
+    
+    for(let i=0; i<96; i= i+4){
+        const oneHour = str.substring(i, i+4);
+        let dec = parseInt(oneHour, 2)
+        hex += dec.toString(16)
+    }
+    
+    if(hex.length === 24){
+        return hex;
+    } else{
+        console.error("Something went wrong");
+        return false;
+    }
 }
 
 export const hexToBin = (str) => {
-    let decimal = parseInt(str,16);
-    return decimal.toString(2)
+    if(str.length !== 24){
+        console.log("Invalid Hex Time format");
+        return false;
+    }
+    
+    let bin = "";
+    
+    for(let i=0; i< 24; i++){
+        const dec = parseInt(str[i], 16);
+        bin += dec.toString(2);
+    }
+    
+    if(bin.length === 96){
+        return bin
+    } else{
+        console.log("something went wrong")
+        return false;
+    }
 }
