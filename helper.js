a7d7859268e404e1cab4003b69982071d7ec9cb3
bhasker-qocial <97150566+bhasker-qocial@users.noreply.github.com>
2022-10-10 01:11:16 +0530

    local to utc time conversion

--- helper.js
+++ helper.js
@@ -57,3 +57,30 @@ export const hexToBin = (str) => {
         return false;
     }
 }
+
+
+  export const localToUtc = (d = new Date()) => {
+    const options = {
+      year: 'numeric', month: 'numeric', day: 'numeric',
+      hour: 'numeric', minute: 'numeric', second: 'numeric',
+      hour12: false,
+      timeZone: 'UTC',
+    }
+
+    const intlDate = new Intl.DateTimeFormat("en-US", options).format(d);
+
+    let utcArr = intlDate.split(",").map(x => x.trim());
+
+    const dateArr = utcArr[0].split("/"); // * M/D/Y
+    const timeArr = utcArr[1].split(":"); //H:M:S
+
+    const utcDate = new Date();
+
+    // set Date & Time
+    utcDate.setDate(dateArr[1]);
+    utcDate.setMonth(dateArr[0]-1);
+    utcDate.setFullYear(dateArr[2]);
+    utcDate.setHours(timeArr[0], timeArr[1], timeArr[2])
+
+    return { utcDate }
+  }
